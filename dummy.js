1251e1489257a9f1699531dd46147efdff8370be
Chandrasekhar Pasupuleti <chandrasekhar.pasupuleti@gmail.com>
2017-11-09 19:39:13 +0530

    Update dummy.js

--- dummy.js
+++ dummy.js
@@ -1,33 +1,41 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import { PieChart } from 'react-d3-basic';
+import { PieTooltip } from 'react-d3-tooltip';
 
-import UsersChart from './UsersChart.jsx'
 
-export default class UsersInfo extends Component {
+export default class UsersChat extends Component {
+	constructor(props) {
+		super(props);
+
+	}
+	value(d) {
+	    return d.BMI;
+	}
+
+    name(d) {
+      return d.city;
+    }
+
 	render() {
-		const users = this.props.users;
-		const chartSeries = [
-	      {
-	        "field": "West Josiemouth",
-	        "name": "West Josiemouth"
-	      },
-	      {
-	        "field": "Annatown",
-	        "name": "Annatown"
-	      },
-	      {
-	        "field": "South Eldredtown",
-	        "name": "South Eldredtown"
-	      },
-	      {
-	        "field": "Koeppchester",
-	        "name": "Koeppchester"
-	      }
-	    ];
+		const generalChartData = this.props.users;
+		const width = this.props.width || 300;
+	    const height = this.props.height || 300;
+	    const innerRadius = 20;
+	    const chartSeries = this.props.chartSeries;
 
 		return (
-			<div id="users-info">
-				<UsersChart users={users} chartSeries={chartSeries} width={300} height={300}/>
+			<div id="users-chart">
+				<PieTooltip
+			      data= {generalChartData}
+			      width= {width}
+			      height= {height}
+			      chartSeries= {chartSeries}
+			      value = {this.value}
+			      name = {this.name}
+			      innerRadius = {innerRadius}
+			      title = "DOnut"
+				/>
 			</div>
-		);
+		)
 	}
-} 
+}
