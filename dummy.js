d1a7c9631c7d7747b3ae8265b55beaf01e8ba169
Chandrasekhar Pasupuleti <chandrasekhar.pasupuleti@gmail.com>
2017-11-10 19:29:01 +0530

    Update dummy.js

--- dummy.js
+++ dummy.js
@@ -1,76 +1,4 @@
-import React, { Component } from 'react';
-import { PieTooltip, SimpleTooltip } from 'react-d3-tooltip';
-
-
-export default class UsersChat extends Component {
-	constructor(props) {
-		super(props);
-
-	}
-	value(d) {
-	    return d.BMI;
-	}
-
-    name(d) {
-      return d.city;
-    }
-
-	render() {
-		const width = this.props.width || 300;
-	    const height = this.props.height || 300;
-
-		return (
-			<div id="users-chart">
-				<PieTooltip
-				  {...this.props}
-			      width= {width}
-			      height= {height}
-			      value = {this.value}
-			      name = {this.name}
-			      innerRadius = {20}
-			      title = "DOnut"
-			      swatchShape = "circle"
-				>
-					<SimpleTooltip />
-				</PieTooltip>
-			</div>
-		)
-	}
-}
-
-
-
-
-
-
-import React, {Component} from 'react'
-
-import UsersList from './UsersList.jsx'
-import UsersChart from './UsersChart.jsx'
-
-export default class UsersInfo extends Component {
-	render() {
-		const users = this.props.users;
-		const chartSeries = [
-	      {
-	        "field": "West Josiemouth",
-	        "name": "West Josiemouth"
-	      },
-	      {
-	        "field": "Annatown",
-	        "name": "Annatown"
-	      },
-	      {
-	        "field": "South Eldredtown",
-	        "name": "South Eldredtown"
-	      },
-	      {
-	        "field": "Koeppchester",
-	        "name": "Koeppchester"
-	      }
-	    ];
-
-		return (
+return (
 			<div id="users-info">
 				<UsersList users={users}/>
 				<UsersChart 
@@ -82,5 +10,3 @@ export default class UsersInfo extends Component {
 				/>
 			</div>
 		);
-	}
-} 
