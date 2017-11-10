e3752234e82e10f23655eeec87d3efa63abf1692
Chandrasekhar Pasupuleti <chandrasekhar.pasupuleti@gmail.com>
2017-11-10 09:31:51 +0530

    Update dummy.js

--- dummy.js
+++ dummy.js
@@ -1,38 +1,86 @@
-[
-		{
-			name: "Lavon Hilll I",
-			BMI: 60.57,
-			age: 12,
-			birthday: "1994-10-26T00:00:00.000Z",
-			city: "Annatown",
-			married: true,
-			index: 1
-		},
-		{
-			name: "Clovis Pagac",
-			BMI: 24.28,
-			age: 26,
-			birthday: "1995-11-10T00:00:00.000Z",
-			city: "South Eldredtown",
-			married: false,
-			index: 3
-		},
-		{
-			name: "Gaylord Paucek",
-			BMI: 24.41,
-			age: 30,
-			birthday: "1975-06-12T00:00:00.000Z",
-			city: "Koeppchester",
-			married: true,
-			index: 5
-		},
-		{
-			name: "Ashlynn Kuhn MD",
-			BMI: 23.77,
-			age: 32,
-			birthday: "1985-08-09T00:00:00.000Z",
-			city: "West Josiemouth",
-			married: false,
-			index: 6
-		}
-	]
+import React, { Component } from 'react';
+import { PieTooltip, SimpleTooltip } from 'react-d3-tooltip';
+
+
+export default class UsersChat extends Component {
+	constructor(props) {
+		super(props);
+
+	}
+	value(d) {
+	    return d.BMI;
+	}
+
+    name(d) {
+      return d.city;
+    }
+
+	render() {
+		const width = this.props.width || 300;
+	    const height = this.props.height || 300;
+
+		return (
+			<div id="users-chart">
+				<PieTooltip
+				  {...this.props}
+			      width= {width}
+			      height= {height}
+			      value = {this.value}
+			      name = {this.name}
+			      innerRadius = {20}
+			      title = "DOnut"
+			      swatchShape = "circle"
+				>
+					<SimpleTooltip />
+				</PieTooltip>
+			</div>
+		)
+	}
+}
+
+
+
+
+
+
+import React, {Component} from 'react'
+
+import UsersList from './UsersList.jsx'
+import UsersChart from './UsersChart.jsx'
+
+export default class UsersInfo extends Component {
+	render() {
+		const users = this.props.users;
+		const chartSeries = [
+	      {
+	        "field": "West Josiemouth",
+	        "name": "West Josiemouth"
+	      },
+	      {
+	        "field": "Annatown",
+	        "name": "Annatown"
+	      },
+	      {
+	        "field": "South Eldredtown",
+	        "name": "South Eldredtown"
+	      },
+	      {
+	        "field": "Koeppchester",
+	        "name": "Koeppchester"
+	      }
+	    ];
+
+		return (
+			<div id="users-info">
+				<UsersList users={users}/>
+				<UsersChart 
+					data={users} 
+					chartSeries={chartSeries} 
+					width={300} 
+					height={300}
+					pieTextShow={false}
+				/>
+			</div>
+		);
+	}
+} 
